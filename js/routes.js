9df84f52dc6b34c1c4f471ebbefcfabf06a6facf
Moheb Yasser <fast.pro.apps@gmail.com>
2017-01-31 22:57:48 -0800

    Add files via upload

---
+++ js/routes.js
@@ -0,0 +1,35 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('fastProApps', {
+    url: '/main',
+    templateUrl: 'templates/fastProApps.html',
+    controller: 'fastProAppsCtrl'
+  })
+
+  .state('newApps', {
+    url: '/new',
+    templateUrl: 'templates/newApps.html',
+    controller: 'newAppsCtrl'
+  })
+
+  .state('page', {
+    url: '/page3',
+    templateUrl: 'templates/page.html',
+    controller: 'pageCtrl'
+  })
+
+$urlRouterProvider.otherwise('/main')
+
+  
+
+});
